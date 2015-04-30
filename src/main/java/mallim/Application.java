3fc95e982e24c523200be75c4a7a368b85cd9a72
Ian Lim <mallim.ink@gmail.com>
2015-04-30 22:36:42 +0800

    initial check-in

---
+++ src/main/java/mallim/Application.java
@@ -0,0 +1,15 @@
+package mallim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/mallim/Greeting.java
@@ -0,0 +1,13 @@
+package mallim;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Greeting {
+    private final long id;
+    private final String content;
+}
